77ab244ca72359da57746bb3831d4240ecb9c0c7
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-04-25 16:16:20 -0500

    fix horrible sql update bug

--- sql/updates/world/master/2022_06_24_00_world.sql
+++ sql/updates/world/master/2022_06_24_00_world.sql
@@ -87,7 +87,7 @@ INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `ExtendedCost`, `
 -- SmartScripts
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` IN (72623, 73701);
 
-DELETE FROM `smart_scripts` WHERE `source_type` = (0 AND `entryOrGuid` = 72623) OR (0 AND `entryOrGuid` = 73701) OR (9 AND `entryOrGuid` = 7370100);
+DELETE FROM `smart_scripts` WHERE (`source_type` = 0 AND `entryOrGuid` = 72623) OR (`source_type` = 0 AND `entryOrGuid` = 73701) OR (`source_type` = 9 AND `entryOrGuid` = 7370100);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (72623, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 11, 148466, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'OnRespawn - Cast spell "Holy Channeling" - to self'),
 (73701, 0, 0, 0, 34, 0, 100, 0, 2, 1, 0, 0, 80, 7370100, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'OnMovementInform PathID 1 - run ActionList - to self'),
