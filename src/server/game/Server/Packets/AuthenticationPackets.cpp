24fd9d32a30d566c47364a0c3403aa99106f6006
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-04-26 08:23:10 -0500

    temp ignore deprecated rsa functions in gcc build

--- src/server/game/Server/Packets/AuthenticationPackets.cpp
+++ src/server/game/Server/Packets/AuthenticationPackets.cpp
@@ -21,7 +21,12 @@
 #include "CryptoHash.h"
 #include "HMAC.h"
 #include "ObjectMgr.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #include "LegacyRSA.h"
+#pragma GCC diagnostic pop
+
 #include "Util.h"
 
 ByteBuffer& operator<<(ByteBuffer& data, WorldPackets::Auth::VirtualRealmNameInfo const& virtualRealmInfo)
