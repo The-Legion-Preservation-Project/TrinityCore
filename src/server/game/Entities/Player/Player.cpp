3a1dbd3c96789551dc19800d1c7a8f60572fb5fc
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-05-06 09:45:28 -0500

    fix build

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1566,9 +1566,6 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             }
 
             SendDirectMessage(transferPending.Write());
-
-            RemovePlayerLocalFlag(PLAYER_LOCAL_FLAG_OVERRIDE_TRANSPORT_SERVER_TIME);
-            SetTransportServerTime(0);
         }
 
         // remove from old map now
