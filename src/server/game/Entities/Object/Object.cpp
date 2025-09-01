3c85e24f280227620c2a163385a613e8447653a7
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2025-09-01 11:41:18 -0500

    fix build

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -46,6 +46,7 @@
 #include "SpellAuraEffects.h"
 #include "SpellMgr.h"
 #include "SpellPackets.h"
+#include "StringConvert.h"
 #include "TemporarySummon.h"
 #include "Totem.h"
 #include "Transport.h"
